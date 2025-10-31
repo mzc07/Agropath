27ca82c1a7b8b8abd1ebe996daf074894fe03b1a
mzc07 <martinsantiagoariza3@gmail.com>
2025-10-31 17:22:19 -0500

    Actualizacion del menu y limpieza de consola

--- src/menu_principal.py
+++ src/menu_principal.py
@@ -1,10 +1,14 @@
+import os
 from fincas_produccion import ver_fincas
 from analisis_produccion import regresion_produccion
 from repositorio_abrir import abrir_repositorio
 
+def limpiar_consola():
+    os.system('cls' if os.name=='nt' else 'clear')
+
 def menu_principal():
 
-        print("Agropath - Menú Principal")
+        print("=== Agropath - Menú Principal ===")
         print("1. Ver fincas de producción.")
         print("2. Ver centros de acopio.")
         print("3. Ver puertos.")
@@ -14,30 +18,37 @@ def menu_principal():
         print("0. Salir.")
 
 if __name__ == "__main__":
-    menu_principal()
-    try:
-        entrada_usuario = int(input("Ingrese su opción: "))
-    except ValueError:
-        print("Error: Ingrese datos numericos.")
-    except IndexError:
-        print("Error: Entrada fuera del rango.")
-    except KeyboardInterrupt:
-        print("Error: Interrupcion del teclado.")
-    else:
-        match entrada_usuario:
-            case 1:
-                ver_fincas()
-            case 2:
-                None
-            case 3:
-                None
-            case 4:
-                None
-            case 5:
-                regresion_produccion()
-            case 6:
-                abrir_repositorio()
-            case 0:
-                None
-            case _:
-                print("Error: Ingrese una opción valida.")
+    while True:
+        limpiar_consola()
+        menu_principal()
+        try:
+            entrada_usuario = int(input("Ingrese su opción: "))
+        except ValueError:
+            print("Error: Ingrese datos numericos.")
+            print("Presione enter para continuar: ")
+            continue
+        except IndexError:
+            print("Error: Entrada fuera del rango.")
+            print("Presione enter para continuar: ")
+            continue
+        except KeyboardInterrupt:
+            print("Error: Interrupcion del teclado.")
+            break
+        else:
+            match entrada_usuario:
+                case 1:
+                    ver_fincas()
+                case 2:
+                    None
+                case 3:
+                    None
+                case 4:
+                    None
+                case 5:
+                    regresion_produccion()
+                case 6:
+                    abrir_repositorio()
+                case 0:
+                    break
+                case _:
+                        print("Error: Ingrese una opción valida.")
